55fa3c5f13fb330cb6c79fa55b83b674cb58e74c
Rage Layasree <layasreerage@gmail.com>
2025-08-05 00:31:00 +0530

    Add files via upload

---
+++ is_prime.py
@@ -0,0 +1,11 @@
+num=int(input('Enter a number='))
+count=0
+N=0
+for i in range (1,num+1):
+    if(num%i ==0):
+        count=count+1
+if (count==2):
+    print('Given number is prime')
+else:
+    print('Given number is not prime')
+    
